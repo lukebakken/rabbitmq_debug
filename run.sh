9cb28c7ba50a2f0014d2a0ece8d8c889da25e108
Luke Bakken <luke@bakken.io>
2020-09-21 09:50:42 -0700

    Removed unused entities

--- run.sh
+++ run.sh
@@ -6,11 +6,15 @@ set -o nounset
 PATH="$HOME/development/rabbitmq/umbrella/deps/rabbitmq_server_release/sbin:$PATH"
 
 declare -r hostname='shostakovich'
+declare -ri port=15672
 declare -r n1="rabbit-1@$hostname"
 declare -r n2="rabbit-2@$hostname"
 declare -r n3="rabbit-3@$hostname"
 declare -r rpc_flood_pid_file="$(mktemp)"
 declare -r rpc_flood_client_pid_file="$(mktemp)"
+
+declare -r ex_unroutable='unroutable'
+declare -r q_unroutable_messages='unroutable_messages'
 
 trap "{ rm -f $rpc_flood_pid_file $rpc_flood_client_pid_file; }" EXIT
 
@@ -22,22 +26,28 @@ source "$PWD/venv/bin/activate"
 setup_cluster()
 {
     set +o errexit
-    # enable drop_unroutable_metric feature
     rabbitmqctl -n "$n1" enable_feature_flag drop_unroutable_metric
 
-    # setup admin
     rabbitmqctl -n "$n1" add_user admin administrator
     rabbitmqctl -n "$n1" set_user_tags admin administrator
     rabbitmqctl -n "$n1" set_permissions --vhost / admin '.*' '.*' '.*'
 
-    # setup user
     rabbitmqctl -n "$n1" add_user user password
     rabbitmqctl -n "$n1" set_user_tags user administrator
     rabbitmqctl -n "$n1" set_permissions --vhost / user '.*' '.*' '.*'
-
-    # setup policy
-    rabbitmqctl -n "$n1" set_policy ha '^(?!amq\.).*' '{"ha-mode":"all", "ha-sync-mode": "automatic"}' --priority 10 --apply-to queues
     set -o errexit
+
+    rabbitmqctl -n "$n1" set_policy 'ha_rpc' '^rpc_' "{\"ha-mode\":\"all\",\"ha-sync-mode\":\"automatic\",\"alternate-exchange\":\"$ex_unroutable\"}" --priority 10 --apply-to queues
+    rabbitmqctl -n "$n1" set_policy 'ha_unroutable' "^$q_unroutable_messages$" "{\"ha-mode\":\"all\",\"ha-sync-mode\":\"automatic\"}" --priority 10 --apply-to queues
+
+    # create unroutable exchange
+    rabbitmqadmin declare exchange name="$ex_unroutable" type='fanout' auto_delete='false' durable='true'
+
+    # create unroutable_messages queue
+    rabbitmqadmin declare queue name="$q_unroutable_messages" auto_delete='false' durable='true' arguments='{"x-message-ttl": 300000}'
+
+    # bind unroutable_messages to unroutable exchange
+    rabbitmqadmin declare binding source="$ex_unroutable" destination="$q_unroutable_messages" destination_type='queue'
 }
 
 check_cluster()
@@ -109,6 +119,7 @@ genload
 
 echo '[INFO] sleeping 60 seconds'
 sleep 60
+
 # rabbitmqctl -n "$n2" shutdown
 
 echo '[INFO] sleeping 10 seconds'
